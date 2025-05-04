819d9d1de2b5c409545e83295f2da714ef8dec70
Phuong Nguyen <phuongnt006@gmail.com>
2025-05-04 11:42:04 +0700

    [Au_D1]_Init Project

---
+++ tests/example.spec.ts
@@ -0,0 +1,30 @@
+import { test, expect } from '@playwright/test';
+
+test.beforeAll('Before all',() => {
+    console.log("Before all");
+})
+test.beforeEach('Before each',() => {
+    console.log("Before each");
+});
+test('has title 1', async ({page}) =>{
+    console.log("has title 1")
+});
+test('has title 2', async ({page}) =>{
+    console.log("has title 2")
+});
+
+test.afterEach('after each',() => {
+    console.log("after each");
+})
+test.afterAll('after all',() => {
+    console.log("after all");
+})
+test.describe("group all",() => {
+    test('has title 1', async ({page}) =>{
+        console.log("has title 1")
+    console.log("has title 1");
+});
+test('has title 2', async ({page}) =>{
+    console.log("has title 2")
+});
+})
