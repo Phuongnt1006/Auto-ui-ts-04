3be2d17dab686dc1a43bd935e477467a25c8b39a
Phuong Nguyen <phuongnt006@gmail.com>
2025-05-05 17:04:30 +0700

    button 05/05/2025

--- tests/Element/Single -Element/Button.spec.ts
+++ tests/Element/Single -Element/Button.spec.ts
@@ -1,16 +1,32 @@
 import { test, expect } from '@playwright/test';
---
+test.beforeEach('Before each', async ({page}) => {
+await page.goto('https://test-with-me-app.vercel.app/learning/web-elements/elements/button/');
+});
 test('Test button', async ({ page }) => {
-await page.goto('https://test-with-me-app.vercel.app/learning/web-elements/elements/button');
-await clickButton(page, 'Default');
-})
+    let buttons = ['Origin button', 'Default', 'Primary'];
+    for (let button of buttons) {
+         await clickButton(page, button);
+    await expect(page.getByText(`Button ${button} was clicked!`)).toBeVisible();
+    }
+    });
+test('Test role button', async ({ page }) => {
+    await clickRoleButton(page, 'Div button');
+    await expect(page.getByText('Button Div button was clicked!')).toBeVisible();
+    });
+
+test('Test Input button', async ({ page }) => {
+     await clickInputButton(page, 'Input button');
+     await expect(page.getByText('Button Input button was clicked!')).toBeVisible();
+     });
 async function clickButton(page: Page, label: string) {
     let xpath = `//button[.//text()[normalize-space()='${label}']]`;
     await page.click(xpath);
 }
-async function clickButton(page: Page, label: string) {
-    let xpath = '';
+async function clickRoleButton(page: Page, label: string) {
+    let xpath = `//div[@role='button' and normalize-space(text())='Div button']`;
+    await page.click(xpath);
 }
-async function clickButton(page: Page, label: string) {
-    let xpath = '';
+async function clickInputButton(page: Page, label: string) {
+    let xpath = `//input[@type='button' and @value[normalize-space()='${label}']]`;
+    await page.click(xpath);
 }
